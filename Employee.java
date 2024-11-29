614a4367eaa838f41fdfde8292928f017fa8931e
Maryam Khan <99869423+MaryamKhan23@users.noreply.github.com>
2024-11-30 00:32:44 +0300

    Password hashing before storing
    changes made to hash the password before being stored using PasswordUtil

--- Employee.java
+++ Employee.java
@@ -41,7 +41,9 @@ public abstract class Employee
     public String getEmail() {return email;}
     public void setEmail(String email) { this.email = email;}
     public String getPassword() {return password;}
-    public void setPassword(String password) { this.password = password;}
+    public void setPassword(String password){
+        this.password = PasswordUtil.hashPassword(password);
+    }
 
     public abstract void Menu();
 }
